5e7e33f22f44eb18518f6fe623a76d2d0e487dda
Tom Fream <tomfream@gmail.com>
2021-04-07 19:35:36 -0600

    word search pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Pair programming with Mitch Kenward and Tom Fream
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  if (letters.length === 0) return false;
+
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+    }
+  // Transpose
+  let finalArray = [];
+  let holderArray = [];
+  let count = 0;
+    
+  for (let i = 0; i < letters[0].length; i++) {
+    for (const elm of letters) {
+      holderArray.push(elm[count]);
+    }
+    finalArray.push(holderArray);
+    count += 1;
+    holderArray = [];
+  }
+
+  const verticalJoin = finalArray.map(ls => ls.join(''));
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
